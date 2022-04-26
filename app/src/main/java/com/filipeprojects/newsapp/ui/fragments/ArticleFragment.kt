edba361fdedfb5c117f5e426a091a87ff4f3a986
Filipedias0 <50221162+Filipedias0@users.noreply.github.com>
2022-04-26 14:47:20 -0300

    Basic layout setup

---
+++ app/src/main/java/com/filipeprojects/newsapp/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,7 @@
+package com.filipeprojects.newsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.filipeprojects.newsapp.R
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+}
---
+++ app/src/main/java/com/filipeprojects/newsapp/ui/fragments/SearchNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.filipeprojects.newsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.filipeprojects.newsapp.R
+
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+}
