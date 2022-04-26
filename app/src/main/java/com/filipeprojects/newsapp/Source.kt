e6eb96dd3f38be69fb007e5143b5019daa3e46ae
Filipedias0 <50221162+Filipedias0@users.noreply.github.com>
2022-04-26 16:15:29 -0300

    Retrofit setup

---
+++ app/src/main/java/com/filipeprojects/newsapp/Source.kt
@@ -0,0 +1,6 @@
+package com.filipeprojects.newsapp
+
+data class Source(
+    val id: Any,
+    val name: String
+)
---
+++ app/src/main/java/com/filipeprojects/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.filipeprojects.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "218242f80ee44ce9a00e454331e2d804"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
