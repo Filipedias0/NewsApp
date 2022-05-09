58e54eba64a582dafba6221418e1152ada46802a
FilipeDiasAndrade <50221162+Filipedias0@users.noreply.github.com>
2022-05-09 12:00:47 -0300

    Fix saving news

--- app/src/main/java/com/filipeprojects/newsapp/db/ArticleDao.kt
+++ app/src/main/java/com/filipeprojects/newsapp/db/ArticleDao.kt
@@ -8,11 +8,11 @@ import com.filipeprojects.newsapp.models.Article
 interface ArticleDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun upsert(article: Article): Long
+    suspend fun upsert(article: Article): Long
 
     @Query("SELECT * FROM articles")
     fun getAllArticles(): LiveData<List<Article>>
 
     @Delete
-    fun deleteArticle(article: Article)
+    suspend fun deleteArticle(article: Article)
 }
